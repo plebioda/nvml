pmem/pmdk

obj: fix lock order issue due to recycler changes
This patch reverts the decision to turn runs back into free chunks
immediately after deallocation, and with that, the code that locked
runs before buckets. The overall performance/fragmentation should be
roughly identical.

This patch is so voluminous due to fine balance required to keep
helgrind/drd happy... The relatively modest changes I've made broke
the happens-before relations of chunk headers, and many attempts at
using ANNOTATE_HAPPENS_BEFORE/ANNOTATE_HAPPENS_AFTER failed. I caved,
and added what one might consider an extranous lock, but luckily in
a place that isn't often called - only when the entire heap
is empty and the algorithm is looking everywhere for free memory that
could be turned into free chunks.

src/common/valgrind_internal.h
@@ -65,7 +65,38 @@ extern unsigned _On_valgrind;
 #include <valgrind/drd.h>
 #endif
 
+#if defined(USE_VG_HELGRIND)
+
+#define VALGRIND_ANNOTATE_BENIGN_RACE_BEGIN(addr, size) do {\
+	if (On_valgrind) \
+		VALGRIND_HG_DISABLE_CHECKING((addr), (size));\
+} while (0)
+
+#define VALGRIND_ANNOTATE_BENIGN_RACE_END(addr, size) do {\
+	if (On_valgrind) \
+		VALGRIND_HG_ENABLE_CHECKING((addr), (size));\
+} while (0)
+
+#elif defined(USE_VG_DRD)
+
+#define VALGRIND_ANNOTATE_BENIGN_RACE_BEGIN(addr, size) do {\
+	if (On_valgrind) \
+		ANNOTATE_IGNORE_READS_BEGIN();\
+} while (0)
+
+#define VALGRIND_ANNOTATE_BENIGN_RACE_END(addr, size) do {\
+	if (On_valgrind) \
+		ANNOTATE_IGNORE_READS_END();\
+} while (0)
+
+#endif
+
 #if defined(USE_VG_HELGRIND) || defined(USE_VG_DRD)
+
+#define VALGRIND_ANNOTATE_BENIGN_RACE(addr, size) do {\
+	if (On_valgrind) \
+		ANNOTATE_HAPPENS_BEFORE((obj));\
+} while (0)
 
 #define VALGRIND_ANNOTATE_HAPPENS_BEFORE(obj) do {\
 	if (On_valgrind) \
@@ -120,6 +151,10 @@ extern unsigned _On_valgrind;
 #define VALGRIND_ANNOTATE_IGNORE_WRITES_BEGIN() do {} while (0)
 
 #define VALGRIND_ANNOTATE_IGNORE_WRITES_END() do {} while (0)
+
+#define VALGRIND_ANNOTATE_BENIGN_RACE_BEGIN(addr, size) do {} while (0)
+
+#define VALGRIND_ANNOTATE_BENIGN_RACE_END(addr, size) do {} while (0)
 
 #endif
 

plebioda/nvml

src/common/valgrind_internal.h
@@ -65,7 +65,38 @@ extern unsigned _On_valgrind;
 #include <valgrind/drd.h>
 #endif
 
+#if defined(USE_VG_HELGRIND)
+
+#define VALGRIND_ANNOTATE_OVERLOOK_READS_BEGIN(addr, size) do {\
+	if (On_valgrind) \
+		VALGRIND_HG_DISABLE_CHECKING((addr), (size));\
+} while (0)
+
+#define VALGRIND_ANNOTATE_OVERLOOK_READS_END(addr, size) do {\
+	if (On_valgrind) \
+		VALGRIND_HG_ENABLE_CHECKING((addr), (size));\
+} while (0)
+
+#elif defined(USE_VG_DRD)
+
+#define VALGRIND_ANNOTATE_OVERLOOK_READS_BEGIN(addr, size) do {\
+	if (On_valgrind) \
+		ANNOTATE_IGNORE_READS_BEGIN();\
+} while (0)
+
+#define VALGRIND_ANNOTATE_OVERLOOK_READS_END(addr, size) do {\
+	if (On_valgrind) \
+		ANNOTATE_IGNORE_READS_END();\
+} while (0)
+
+#endif
+
 #if defined(USE_VG_HELGRIND) || defined(USE_VG_DRD)
+
+#define VALGRIND_ANNOTATE_BENIGN_RACE(addr, size) do {\
+	if (On_valgrind) \
+		ANNOTATE_HAPPENS_BEFORE((obj));\
+} while (0)
 
 #define VALGRIND_ANNOTATE_HAPPENS_BEFORE(obj) do {\
 	if (On_valgrind) \
@@ -120,6 +151,10 @@ extern unsigned _On_valgrind;
 #define VALGRIND_ANNOTATE_IGNORE_WRITES_BEGIN() do {} while (0)
 
 #define VALGRIND_ANNOTATE_IGNORE_WRITES_END() do {} while (0)
+
+#define VALGRIND_ANNOTATE_OVERLOOK_READS_BEGIN(addr, size) do {} while (0)
+
+#define VALGRIND_ANNOTATE_OVERLOOK_READS_END(addr, size) do {} while (0)
 
 #endif
 